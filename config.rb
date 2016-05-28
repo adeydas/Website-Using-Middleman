3c0270424595d7d61367354dc124825c475d1c9f
Abhishek Dey Das <abhishek@abhis.ws>
2016-05-27 21:23:31 -0700

    Added some more cool stuff

--- config.rb
+++ config.rb
@@ -26,7 +26,7 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  blog.permalink = "blog/{year}/{month}/{day}/{title}.html"
+  blog.permalink = "blog/{category}/{title}.html"
   # Matcher for blog source files
   blog.sources = "blog-posts/{year}-{month}-{day}-{title}.html"
   blog.taglink = "tags/{tag}.html"
--- source/blog-posts/2016-05-28-hello-world.html.markdown
+++ source/blog-posts/2016-05-28-hello-world.html.markdown
@@ -2,5 +2,6 @@
 title: Hello World
 date: 2016-05-28 03:42 UTC
 tags: welcome
+category: Misc
 ---
 Welcome to my blog. In the times to come, this space on my little home page would contain (hopefully) interesting articles on the myraid of things that interest me. So stay tuned!
