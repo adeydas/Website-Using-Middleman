67b1c6d77333fc2f0c286a03465d0b2ab4a9b0be
Abhishek Dey Das <abhishek@abhis.ws>
2016-05-27 20:30:58 -0700

    Basic blog structure working

--- config.rb
+++ config.rb
@@ -11,11 +11,6 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# Set configurations
-config[:js_dir] = 'javascripts'
-config[:css_dir] = 'stylesheets'
-config[:img_dir] = 'images'
-
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
 
@@ -23,16 +18,40 @@ config[:img_dir] = 'images'
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-# General configuration
-
-# Reload the browser automatically whenever files change
-configure :development do
-  # activate :livereload
-end
-
 ###
 # Helpers
 ###
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  blog.sources = "blog-posts/{year}-{month}-{day}-{title}.html"
+  blog.taglink = "tags/{tag}.html"
+  blog.layout = "blog_layout"
+  blog.summary_separator = /(READMORE)/
+  blog.summary_length = 250
+  blog.year_link = "blog/{year}.html"
+  blog.month_link = "blog/{year}/{month}.html"
+  blog.day_link = "blog/{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
